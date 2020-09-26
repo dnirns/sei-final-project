397acdae9a6c33b8d0341dbef6163275adb07a4f
dnirns <daniel.irons@gmail.com>
2020-09-26 14:15:50 +0100

    ready to deploy

--- project/settings.py
+++ project/settings.py
@@ -9,6 +9,7 @@ https://docs.djangoproject.com/en/3.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
+import django_heroku
 
 import os
 
@@ -145,3 +146,5 @@ REST_FRAMEWORK = {
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'frontend', "build", "static"),
 )
+
+django_heroku.settings(locals())
